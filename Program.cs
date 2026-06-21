be8de2740480a9fe5ebac7dd9fa77e57fc1857db
Santiago Echegaray <santiagoecheg@gmail.com>
2026-06-21 03:10:06 -0300

    Creo estructura básica

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿using System;
 
+/*
+sistema de voluntariado desarrollar un sistema de consola, que permita:
+
+*administrar personas de una organización solidaria 
+
+deben existir, voluntarios y coordinadores .
+
+los voluntarios deberán mostrar cantidad de horas trabajadas 
+y los coordinadores áreas asignadas y cantidad de personas a cargo. 
+
+*/
+
+public class Program {
+
+    static void Main(){
+
+        MostrarMenu();
+
+    }
+
+    static public void MostrarMenu(){
+
+        int opcion=0;
+
+        Console.WriteLine("Bienvenido al programa de gestor de voluntarios");
+
+        
+
+    }
+}
